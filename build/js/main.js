66006e4f0d10e75b32323704fa5b0f2bbc5864bf
Bilal-Nizamani <bilalnizamani001@gmail.com>
2023-11-14 10:53:18 +0500

    typescript setup

---
+++ build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let ame = 'Bilawal';
+let a = 123;
+let b = 'bggg';
+let c = 123;
+let d = a * c;
+console.log(ame, a, c / a, c, d);
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+let ame: string = 'Bilawal';
+let a: number = 123
+let b: string = 'bggg'
+let c: number = 123
+let d: number = a * c
+console.log(ame,a,c/a,c,d)
